2e36a5fa27ad7cd44e59493a79b8787ab2192712
XAKEPEHOK <icq-475204@ya.ru>
2014-01-31 04:09:21 +0400

    Глобальное обновление шаблона формы

--- assets/formHelper.js
+++ assets/formHelper.js
@@ -4,42 +4,61 @@ window.leadvertex.form = {};
 window.leadvertex.form.label = function(field,name,form){
     if (!form) form = ''; else if (form<2) form = '';
     $(document).ready(function(){
-        $('label[for=form'+form+'_'+field +']').text(name);
+        $('label[for=lv-form'+form+'-'+field +']').text(name);
     });
 };
 window.leadvertex.form.subLabel = function(field,text,form){
     if (!form) form = ''; else if (form<2) form = '';
     $(document).ready(function(){
-        $('#lv_form'+form+' div.field_'+field +'>.form_field').after('<div class="form_sub_label"><label for="form'+form+'_'+field +'">'+text+'</label></div>');
+        $('#lv-form'+form+' div.lv-row-'+field +'>.lv-field').after('<div class="lv-sub-label"><label for="lv-form'+form+'-'+field +'">'+text+'</label></div>');
     });
 };
 
 window.leadvertex.form.placeholder = function(field,placeholder,form){
     if (!form) form = ''; else if (form<2) form = '';
     $(document).ready(function(){
-        $('#form'+form+'_'+field).attr('placeholder',placeholder);
+        $('#lv-form'+form+'-'+field).attr('placeholder',placeholder);
     });
 };
 window.leadvertex.form.placeholderOnly = function(form){
     if (!form) form = ''; else if (form<2) form = '';
     $(document).ready(function(){
-        $('#lv_form'+form+' .field.field_input').each(function(i,e){
-            $(e).find('.form_label').hide();
-            var $input = $(e).find('.form_field > *');
-            $input.attr('placeholder',$input.attr('data-label'));
+        $('#lv-form'+form+' .lv-row.lv-row-input').each(function(i,e){
+            $(e).find('.lv-label').hide();
+            var $input = $(e).find('.lv-field > *');
+            $input.attr('placeholder',$input.attr('data-label')+($input.attr('data-required') == '1' ? ' *' : ''));
         });
     });
 };
 
+window.leadvertex.form.validation = function($form, data, hasError) {
+    if (hasError) {
+        var errors = '';
+        if ($form.attr('data-validation-by-alert')) {
+            for (var i in data) errors+= data[i][0]+"\n\n";
+            alert(errors);
+        }
+    }
+    return true;
+}
+window.leadvertex.form.validationByAlert = function(form){
+    if (!form) form = ''; else if (form<2) form = '';
+    $(document).ready(function(){
+        var $form = $('#lv-form'+form);
+        $form.attr('data-validation-by-alert',1);
+        $form.find('.lv-error').hide();
+    });
+}
+
 $(document).ready(function(){
-    $('.lv_move').each(function(i,e){
+    $('.lv-move').each(function(i,e){
         var form = $(e).attr('data-form');
         if (!form) form = '';
         var position = $(e).attr('data-position').toString().toLowerCase();
         var field = $(e).attr('data-field').toString().toLowerCase();
-        if (field == 'submit') field = 'div.form_submit';
-        else field = 'div.field_'+field;
-        var $element = $('#lv_form'+form+' '+field);
+        if (field == 'submit') field = 'div.lv-form-submit';
+        else field = 'div.lv-row-'+field;
+        var $element = $('#lv-form'+form+' '+field);
         if (position == 'before') $element.before($(e));
         else $element.after($(e));
     });
